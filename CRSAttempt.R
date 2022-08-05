b89119e66d363736a18ea54e3f6c6542e77303a2
szykozlowski <92408910+szykozlowski@users.noreply.github.com>
2022-08-05 09:39:10 -0700

    Add files via upload

--- CRSAttempt.R
+++ CRSAttempt.R
@@ -1,3 +1,9 @@
+# Szymon Kozlowski
+
+# 8/05/2022
+
+
+# Most of these are useless
 library(ggplot2)
 library(ncdf4)
 library(raster) 
@@ -9,28 +15,46 @@ library(sf)
 library(dplyr)
 library(spData)
 library(spDataLarge)
+
+# Open the netCDF using the ncdf4 package 
 nc_file <- nc_open('MTDCA_201504_201603_36km_V5.nc')
+
+# This line doesn't work
 r <- raster("MTDCA_201504_201603_36km_V5.nc",varname = "VOD")
+
+# Extracting the longitude/latitude/time variables from the netCDF
 lon <- ncvar_get(nc_file, "lon")
 lat <- ncvar_get(nc_file, "lat")
 t <- ncvar_get(nc_file, "time")
 
-
-
+# Turning fill values into 'NA'
 fillvalue <- ncatt_get(nc_file, "VOD", "_FillValue")
 VODARRAY[VODARRAY == fillvalue$value] <- NA
 
+# Getting the 3d array of time/lon/lat into a variable
 VODARRAY <- ncvar_get(nc_file, "VOD")
+
+# Getting a specific day of the array
 VODARRAYSLICE <- VODARRAY[, , 360] 
 dim(VODARRAYSLICE)
 
+# Throwing a day of lon/lat data into a raster, and attempting to project it
 r <- raster(t(VODARRAYSLICE), xmn=min(lon), xmx=max(lon), ymn=min(lat), ymx=max(lat),crs = CRS("+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"))
+
+# Futile attempts of getting the projection to work
 crs(r) <- "EPSG:6931"
-crs(r) <- "EPSG:2002"
 crs(r,proj = TRUE)
-plot(r)
 projection(r)
 p1 <- projectRaster(r, crs="+init=EPSG:6931")
 plot(p1)
 r <- projectRaster(r,crs="+init=EPSG:6931")
 geo_proj = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+# Plotting the raster
+plot(r)
+
+# Clear packages
+detach("package:datasets", unload = TRUE)
+
+# Clear plots
+dev.off()
